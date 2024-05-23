186fe82a33b1bd68515101aec2cc8437cc3f478a
haneul <gksmf3064s@gmail.com>
2024-05-23 18:50:28 +0900

    필요없는 코드 수정

--- src/screens/routes/MainPage/LeftNotice.jsx
+++ src/screens/routes/MainPage/LeftNotice.jsx
@@ -1,20 +1,20 @@
-import styled from 'styled-components';
-import Notice from './LeftNoticeJsx/Notice';
-import UpcomigReleases from './LeftNoticeJsx/UpcomigReleases';
-import Review from './LeftNoticeJsx/Review';
+import styled from "styled-components";
+import Notice from "./LeftNoticeJsx/Notice";
+import UpcomigReleases from "./LeftNoticeJsx/UpcomigReleases";
+import Review from "./LeftNoticeJsx/Review";
 
 const VerticalContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 export default function LeftNotice() {
-    return (
-        <VerticalContainer>
-            <Notice />
-            <UpcomigReleases />
-            <Review />
-        </VerticalContainer>
-    );
+  return (
+    <VerticalContainer>
+      <Notice />
+      <UpcomigReleases />
+      <Review />
+    </VerticalContainer>
+  );
 }
