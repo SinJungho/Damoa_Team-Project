06b561739819df777e111989b86ceef08743540d
SinJungho <zmtls0516@naver.com>
2024-05-28 00:52:50 +0900

    1. 라우터 경로 추가 2. Community 컴포넌트 추가

--- src/screens/Router.jsx
+++ src/screens/Router.jsx
@@ -1,28 +1,33 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Home from './routes/MainPage/Home';
-import Root from './Root';
-import Review from './routes/ReviewPage/Review';
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./routes/MainPage/Home";
+import Root from "./Root";
+import Review from "./routes/ReviewPage/Review";
+import Community from "./routes/CommunityPage/Commnuity";
 
 const router = createBrowserRouter(
-    [
-        {
-            path: '/',
-            element: <Root />,
-            children: [
-                {
-                    path: '',
-                    element: <Home />,
-                },
-                {
-                    path: '/review',
-                    element: <Review />,
-                },
-            ],
-        },
-    ],
+  [
     {
-        basename: `${process.env.PUBLIC_URL}`,
-    }
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "/review",
+          element: <Review />,
+        },
+        {
+          path: "/community",
+          element: <Community />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: `${process.env.PUBLIC_URL}`,
+  }
 );
 
 export default router;
