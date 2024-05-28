3167f206bd543171b9db6027561fffcd456d6ed9
SinJungho <zmtls0516@naver.com>
2024-05-28 21:50:46 +0900

    babel 설정 파일 생성 및 .eslintrc.js 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,28 @@
 module.exports = {
-    parser: '@babel/eslint-parser',
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:jsx-a11y/recommended'],
-    plugins: ['react', 'jsx-a11y', 'react-hooks'],
-    env: {
-        browser: true,
-        node: true,
-        es6: true,
+  parser: "@babel/eslint-parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  plugins: ["react", "jsx-a11y", "react-hooks"],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-    rules: {
-        'jsx-a11y/img-redundant-alt': 'off',
-        'no-console': 'warn',
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-uses-react': 'off',
-        'react/jsx-uses-vars': 'off',
-        'react/jsx-no-undef': 'off',
-        'react/no-unknown-property': 'off',
-    },
+  },
+  // rules: {
+  //     'jsx-a11y/img-redundant-alt': 'off',
+  //     'no-console': 'warn',
+  //     'react/react-in-jsx-scope': 'off',
+  //     'react/jsx-uses-react': 'off',
+  //     'react/jsx-uses-vars': 'off',
+  //     'react/jsx-no-undef': 'off',
+  //     'react/no-unknown-property': 'off',
+  // },
 };
---
+++ src/utils/Router.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./routes/MainPage/Home";
+import Root from "./Root";
+import Review from "./routes/ReviewPage/Review";
+import Community from "./routes/CommunityPage/Commnuity";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "/review",
+          element: <Review />,
+        },
+        {
+          path: "/community",
+          element: <Community />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: `${process.env.PUBLIC_URL}`,
+  }
+);
+
+export default router;
