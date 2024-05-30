578974b5686c7ef4237bb8f83d5a30442f4d2a85
KoesJin <wlstjr2011@naver.com>
2024-05-31 01:01:04 +0900

    회원가입 버튼 생성

--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -2,31 +2,55 @@ import Modal from 'react-modal';
 import LoginForm from './LoginForm';
 import { useState } from 'react';
 import style from '../css/LoginForm.module.css';
+import SignUpForm from './SignUpForm';
 
 export default function LoginPage() {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [loginPageOpen, setloginPageOpen] = useState(false);
+    const [signUpPageOpen, setsignUpPageOpen] = useState(false);
 
-    const openModal = () => {
-        setModalIsOpen(true);
+    const openLoginModal = () => {
+        setloginPageOpen(true);
     };
 
-    const closeModal = () => {
-        setModalIsOpen(false);
+    const closeLoginModal = () => {
+        setloginPageOpen(false);
+    };
+
+    const openSignUpModal = () => {
+        setsignUpPageOpen(true);
+    };
+
+    const closeSignUpModal = () => {
+        setsignUpPageOpen(false);
     };
     return (
         <>
-            <button type="button" onClick={openModal} className={style.loginButton}>
+            <button type="button" onClick={openLoginModal} className={style.button}>
                 로그인
             </button>
+            <button type="button" onClick={openSignUpModal} className={style.button}>
+                회원가입
+            </button>
             <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
+                isOpen={loginPageOpen}
+                onRequestClose={closeLoginModal}
                 className={style.content}
                 style={{
                     overlay: { backgroundColor: 'rgba(33, 33, 33, 0.75)' },
                 }}
             >
                 <LoginForm />
+            </Modal>
+
+            <Modal
+                isOpen={signUpPageOpen}
+                onRequestClose={closeSignUpModal}
+                className={style.signUp}
+                style={{
+                    overlay: { backgroundColor: 'rgba(33, 33, 33, 0.75)' },
+                }}
+            >
+                <SignUpForm />
             </Modal>
         </>
     );
