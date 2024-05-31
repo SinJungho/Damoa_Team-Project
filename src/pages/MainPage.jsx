05660bbb3d99c7b22e98ee6dd2ce68e71ed98866
KoesJin <wlstjr2011@naver.com>
2024-06-01 01:55:39 +0900

    상단 바 고정을 위해 MainPage css 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from 'styled-components';
-import styled from 'styled-components';
 import MainBenner from '../component/MainBennerImg';
 import ContentDisplay from '../component/ContentDisplay';
 import Notice from '../component/NoticeBox';
@@ -12,6 +11,7 @@ import HotContent from '../component/HotContent';
 import app from '../css/App.module.css';
 import Date from '../component/Date';
 import styles from '../css/Layout.module.css';
+import MaingPaddingTop from '../css/Navbar.module.css';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -30,26 +30,28 @@ export default function MainPage() {
     return (
         <>
             <GlobalStyle />
-            <MainBenner />
-            {/* Left Notice */}
-            <div className={styles.layout}>
-                <div className={styles.MainPageverticalContainer}>
-                    <Notice />
-                    <UpcomigReleases />
-                    <Review />
-                </div>
+            <div className={MaingPaddingTop.mianPagePaddingTop}>
+                <MainBenner />
+                {/* Left Notice */}
+                <div className={styles.layout}>
+                    <div className={styles.MainPageverticalContainer}>
+                        <Notice />
+                        <UpcomigReleases />
+                        <Review />
+                    </div>
 
-                {/* Right Notice */}
-                <div className={app.bg}>
-                    <p className={app.title}>어떤 작품을 찾아볼까요?</p>
-                    <Date />
-                    <Language />
-                    <Rating />
-                    <Genre />
-                    <HotContent />
+                    {/* Right Notice */}
+                    <div className={app.bg}>
+                        <p className={app.title}>어떤 작품을 찾아볼까요?</p>
+                        <Date />
+                        <Language />
+                        <Rating />
+                        <Genre />
+                        <HotContent />
+                    </div>
                 </div>
+                <ContentDisplay />
             </div>
-            <ContentDisplay />
         </>
     );
 }
