b4f0626158c9f85564cf17bbe384b9480c054b73
KoesJin <wlstjr2011@naver.com>
2024-06-01 01:34:44 +0900

    기대되는 개봉작 페이지 추가

--- src/pages/Upcoming.jsx
+++ src/pages/Upcoming.jsx
@@ -16,7 +16,7 @@ export default function UpcomingPage() {
     return (
         <>
             <GlobalStyle />
-            <h1>여긴 기대되는 개봉작 컴포넌트가 들어올겁니다.</h1>
+            <h1>여긴 기대되는 개봉작 페이지입니다.</h1>
         </>
     );
 }
