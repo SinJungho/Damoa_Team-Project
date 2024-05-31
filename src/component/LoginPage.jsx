ffc82d254060cdb2e649ff09731b102194f7f58d
KoesJin <wlstjr2011@naver.com>
2024-05-31 21:57:54 +0900

    Modal 내용 수정 Login - SignUp 간 이동 가능

--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -5,24 +5,24 @@ import style from '../css/LoginForm.module.css';
 import SignUpForm from './SignUpForm';
 
 export default function LoginPage() {
-    const [loginPageOpen, setloginPageOpen] = useState(false);
-    const [signUpPageOpen, setsignUpPageOpen] = useState(false);
+    const [loginPageOpen, setLoginPageOpen] = useState(false);
+    const [signUpPageOpen, setSignUpPageOpen] = useState(false);
 
     const openLoginModal = () => {
-        setloginPageOpen(true);
-    };
-
-    const closeLoginModal = () => {
-        setloginPageOpen(false);
+        setLoginPageOpen(true);
+        setSignUpPageOpen(false);
     };
 
     const openSignUpModal = () => {
-        setsignUpPageOpen(true);
+        setLoginPageOpen(false);
+        setSignUpPageOpen(true);
     };
 
-    const closeSignUpModal = () => {
-        setsignUpPageOpen(false);
+    const closeModal = () => {
+        setLoginPageOpen(false);
+        setSignUpPageOpen(false);
     };
+
     return (
         <>
             <button type="button" onClick={openLoginModal} className={style.button}>
@@ -33,24 +33,34 @@ export default function LoginPage() {
             </button>
             <Modal
                 isOpen={loginPageOpen}
-                onRequestClose={closeLoginModal}
+                onRequestClose={closeModal}
                 className={style.content}
                 style={{
                     overlay: { backgroundColor: 'rgba(33, 33, 33, 0.75)' },
                 }}
             >
-                <LoginForm />
+                <LoginForm openSignUpModal={openSignUpModal} />
             </Modal>
 
             <Modal
                 isOpen={signUpPageOpen}
-                onRequestClose={closeSignUpModal}
+                onRequestClose={closeModal}
                 className={style.signUp}
                 style={{
                     overlay: { backgroundColor: 'rgba(33, 33, 33, 0.75)' },
+                    content: {
+                        top: '50%',
+                        left: '50%',
+                        right: 'auto',
+                        bottom: 'auto',
+                        marginRight: '-50%',
+                        transform: 'translate(-50%, -50%)',
+                        maxHeight: '90vh', // 최대 높이 설정
+                        overflowY: 'auto', // 내용이 넘칠 경우 스크롤
+                    },
                 }}
             >
-                <SignUpForm />
+                <SignUpForm openLoginModal={openLoginModal} />
             </Modal>
         </>
     );
