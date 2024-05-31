7146dc5a668a60f890b891f40c09703c615fa5bf
KoesJin <wlstjr2011@naver.com>
2024-05-31 22:04:19 +0900

    기대되는 개봉작 페이지(미구현) 추가

---
+++ src/pages/Upcoming.jsx
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box; 
+}
+body {
+    background-color: #141414;
+    margin: 0 auto;
+}
+`;
+
+export default function UpcomingPage() {
+    return (
+        <>
+            <GlobalStyle />
+        </>
+    );
+}
