eede8f2056b7189888a6c6bbb4480e2a46c06789
KoesJin <wlstjr2011@naver.com>
2024-06-05 10:30:33 +0900

    자체 데이터 삭제 -> DB연동 완료

--- src/component/PostDetail.jsx
+++ src/component/PostDetail.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import communityData from '../utils/communityData';
 import styles from '../css/PostDetail.module.css';
 
 const PostDetail = () => {
