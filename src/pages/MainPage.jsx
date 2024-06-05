aba2c579221eb94d7e4fba0b31393a07d0f1207a
KoesJin <wlstjr2011@naver.com>
2024-06-05 13:44:47 +0900

    삭제한 css 경로 제거

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -11,7 +11,6 @@ import HotContent from '../component/HotContent';
 import app from '../css/App.module.css';
 import Date from '../component/Date';
 import styles from '../css/Layout.module.css';
-import MaingPaddingTop from '../css/Navbar.module.css';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -30,28 +29,26 @@ export default function MainPage() {
     return (
         <>
             <GlobalStyle />
-            <div className={MaingPaddingTop.mianPagePaddingTop}>
-                <MainBenner />
-                {/* Left Notice */}
-                <div className={styles.layout}>
-                    <div className={styles.MainPageverticalContainer}>
-                        <Notice />
-                        <UpcomigReleases />
-                        <Review />
-                    </div>
-
-                    {/* Right Notice */}
-                    <div className={app.bg}>
-                        <p className={app.title}>어떤 작품을 찾아볼까요?</p>
-                        <Date />
-                        <Language />
-                        <Rating />
-                        <Genre />
-                        <HotContent />
-                    </div>
+            <MainBenner />
+            {/* Left Notice */}
+            <div className={styles.layout}>
+                <div className={styles.MainPageverticalContainer}>
+                    <Notice />
+                    <UpcomigReleases />
+                    <Review />
                 </div>
-                <ContentDisplay />
+
+                {/* Right Notice */}
+                <div className={app.bg}>
+                    <p className={app.title}>어떤 작품을 찾아볼까요?</p>
+                    <Date />
+                    <Language />
+                    <Rating />
+                    <Genre />
+                    <HotContent />
+                </div>
             </div>
+            <ContentDisplay />
         </>
     );
 }
