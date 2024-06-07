6cbad9196396be8144a7f69a8154b30ae6560b1d
KoesJin <wlstjr2011@naver.com>
2024-06-07 21:33:18 +0900

    공지사항 css 변경

--- src/component/NoticeBox.jsx
+++ src/component/NoticeBox.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styles from '../css/Notice.module.css';
+import styles from '../css/Notice.module.css'; // Ensure this path is correct
 import axios from 'axios';
 
 export default function NoticeBox() {
